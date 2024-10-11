f0dad11a3a086500db882516edbc6f075be1806d
eldivategar <tegareldiva@students.amikom.ac.id>
2024-10-11 17:20:57 +0700

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import hypercorn.asyncio
 from middleware.cors import add_cors_middleware
 from diagnoses.router import router
 
@@ -12,7 +13,3 @@ app = FastAPI(
 app.include_router(router, tags=['diagnose'])
 
 add_cors_middleware(app)
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=5000)
